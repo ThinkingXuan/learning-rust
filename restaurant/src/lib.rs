3638011fd088d16247c056eaf43c55a7a0cf3992
ThinkingXuan <1454739828@qq.com>
2023-05-16 02:39:13 +0000

    save

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -19,3 +19,8 @@ pub fn eat_at_restaurant() {
     // 相对路径
     front_of_house::hosting::add_to_waitlist();
 }
+
+use crate::front_of_house::hosting;
+pub fn eat_at_restaurant2() {
+    hosting::add_to_waitlist();
+}
