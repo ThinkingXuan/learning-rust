2ebe0750e2d0760dd3415fe8d118fc5b806e9ad8
ThinkingXuan <1454739828@qq.com>
2023-05-17 08:02:24 +0000

    hashmap test

---
+++ hashmap_test/src/main.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+
+    let team = String::from("Blue");
+
+    let score = scores.get(&team).copied().unwrap_or(0);
+
+    println!("{}", score);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+}
